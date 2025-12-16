fab9aa51d697a361306072e276bbd6fea3bb1b85
MANISH6148 <manishbobburi6148@gmail.com>
2025-12-16 11:32:30 +0530

    feat(routes): add user routes

---
+++ src/routes/v1/users.routes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+const { userController } = require("../../controllers");
+
+router.post("/", 
+    userController.createUser
+);
+
+router.get("/:id",
+    userController.getUser
+);
+
+module.exports = router;
