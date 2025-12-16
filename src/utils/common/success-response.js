d85035620e93237f5df0ccb461a1580a3a3047f8
MANISH6148 <manishbobburi6148@gmail.com>
2025-12-16 09:59:07 +0530

    refactor(utils): enforce 2xx status codes in sendSuccess

--- src/utils/common/success-response.js
+++ src/utils/common/success-response.js
@@ -1,11 +1,15 @@
 const { StatusCodes } = require("http-status-codes");
 
-function sendSuccess(res, data, message = "OK", status = StatusCodes.OK) {
+function sendSuccess(res, data, message = "Request successful", status = StatusCodes.OK) {
+    if (status < 200 || status >= 300) {
+        throw new Error("sendSuccess requires a 2xx status code");
+    }
+
     res.status(status).json({
             success: true,
             data,
             message,
-        });
+    });
 }
 
 module.exports = sendSuccess;
