0f83d2c02102ca99bbf4d26db3faa1b587398741
MANISH6148 <manishbobburi6148@gmail.com>
2026-01-05 00:56:47 +0530

    feat(middlewares): add validateAuthRequest and checkAuth

---
+++ src/middleware/auth.middleware.js
@@ -0,0 +1,53 @@
+const { StatusCodes } = require("http-status-codes");
+const { AppError } = require("../utils/errors");
+const { userService } = require("../services");
+
+function validateAuthRequest(req, res, next) {
+    const { email, password } = req.body;
+
+    if (!email) {
+        throw new AppError(
+            "Email is required",
+            StatusCodes.BAD_REQUEST,
+            "EMAIL_REQUIRED"
+        );
+    }
+
+    if (!password) {
+        throw new AppError(
+            "Password is required",
+            StatusCodes.BAD_REQUEST,
+            "PASSWORD_REQUIRED"
+        );
+    }
+
+    next();
+}
+
+async function checkAuth(req, res, next) {
+    try {
+        const token =
+            req.headers["x-access-token"] ||
+            req.headers.authorization?.split(" ")[1];
+
+        if (!token) {
+            throw new AppError(
+                "Authentication token missing",
+                StatusCodes.BAD_REQUEST,
+                "TOKEN_MISSING"
+            );
+        }
+
+        const userId = await userService.isAuthenticated(token);
+        req.user = userId;
+
+        next();
+    } catch (err) {
+        next(err);
+    }
+}
+
+module.exports = {
+    validateAuthRequest,
+    checkAuth,
+};
