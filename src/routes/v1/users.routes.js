8bc5d10ae08f341792e3e4da634e3695d4f3132d
MANISH6148 <manishbobburi6148@gmail.com>
2026-01-20 23:43:11 +0530

    feat(routes): add /me get route

--- src/routes/v1/users.routes.js
+++ src/routes/v1/users.routes.js
@@ -2,13 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 const { userController } = require("../../controllers");
+const { authMiddleware } = require("../../middleware")
 
-router.post("/", 
-    userController.createUser
+router.post("/signup", 
+    userController.signUp
 );
 
-router.get("/:id",
-    userController.getUser
+router.post("/signin",
+    userController.signIn
 );
+
+router.get("/me",
+    authMiddleware.checkAuth,
+    userController.getMe
+)
 
 module.exports = router;
