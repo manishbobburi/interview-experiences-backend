b2564de22d3e5daaca952e9286b5404ada89f0fe
MANISH6148 <manishbobburi6148@gmail.com>
2026-01-20 23:41:28 +0530

    feat(controllers): add getMe function

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,39 +1,44 @@
 const { StatusCodes } = require("http-status-codes");
 
 const { userService } = require("../services");
-const { sendSuccess } = require("../utils/common")
+const { sendSuccess } = require("../utils/common");
 
-async function createUser(req, res, next) {
+async function signUp(req, res, next) {
     try {
-        const user = await userService.createUser(req.body);
-
-        return sendSuccess(
-            res, 
-            user, 
-            "User created successfully", 
-            StatusCodes.CREATED
-        );      
+        const user = await userService.signUp({
+            name: req.body.name,
+            email: req.body.email,
+            passwordHash: req.body.password,
+        });
+        return sendSuccess(res, user, "User signed up successfully", StatusCodes.CREATED);      
     } catch (err) {
         next(err);
     }
 }
 
-async function getUser(req, res, next) {
+async function signIn(req, res, next) {
     try {
-        const user = await userService.getUser(req.params.id);
+        const user = await userService.signIn({
+            email: req.body.email,
+            passwordHash: req.body.password,
+        });
+        return sendSuccess(res, user, "User signed in successfully", StatusCodes.CREATED);
+    } catch (err) {
+        next(err);
+    }
+}
 
-        return sendSuccess(
-            res,
-            user,
-            "User created successfully",
-            StatusCodes.OK
-        );
+async function getMe(req, res, next) {
+    try {
+        const user = await userService.getMe(req.user);
+        return sendSuccess(res, user, "Fetched user successfully", StatusCodes.OK);
     } catch (err) {
         next(err);
     }
 }
 
 module.exports = {
-    createUser,
-    getUser,
+    signUp,
+    signIn,
+    getMe,
 }
