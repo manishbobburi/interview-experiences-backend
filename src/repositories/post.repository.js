6a857fedf86adb916acca09a934a0c4cb79babbe
MANISH6148 <manishbobburi6148@gmail.com>
2026-01-25 17:54:29 +0530

    feat(repositories): implement findPosts and findPostsByUserId methods

--- src/repositories/post.repository.js
+++ src/repositories/post.repository.js
@@ -5,6 +5,24 @@ class PostRepository extends CrudRepository {
     constructor() {
         super(Post);
     }
+
+    async findPosts() {
+        const posts = await Post.findAll({
+            order: [
+                ['createdAt', 'DESC']
+            ]
+        })
+        return posts;
+    }
+    async findPostsByUserId(userId) {
+        const posts = await Post.findAll({
+            where: {userId: userId},
+            order: [
+                ['createdAt', 'DESC']
+            ]
+        });
+        return posts;
+    }
 }
 
 module.exports = PostRepository;
