b1a6e3f331ca4dd9b140ca4bffb22d87d61e3ef0
MANISH6148 <manishbobburi6148@gmail.com>
2026-01-26 17:47:16 +0530

    feat(routes): apply auth middleware to votes routes

--- src/routes/v1/votes.routes.js
+++ src/routes/v1/votes.routes.js
@@ -2,16 +2,20 @@ const express = require("express");
 const router = express.Router();
 
 const { voteController } = require("../../controllers");
+const { authMiddleware } = require("../../middleware");
 
 router.post("/upVote",
+     authMiddleware.checkAuth,
      voteController.upVote
 );
 
 router.post("/downVote",
-    voteController.downVote,
+     authMiddleware.checkAuth,
+     voteController.downVote,
 );
 
 router.delete("/removeVote",
+     authMiddleware.checkAuth,
      voteController.removeVote
 );
 
