a9115d338d9f49f93ae2a1647cf7f8cc242d11d0
MANISH6148 <manishbobburi6148@gmail.com>
2026-01-30 00:08:16 +0530

    fix(queries): include User model in posts query

--- src/repositories/post.repository.js
+++ src/repositories/post.repository.js
@@ -1,5 +1,5 @@
 const CrudRepository = require("./crud.repository");
-const { Post } = require("../models");
+const { Post, User } = require("../models");
 
 class PostRepository extends CrudRepository {
     constructor() {
@@ -10,15 +10,37 @@ class PostRepository extends CrudRepository {
         const posts = await Post.findAll({
             order: [
                 ['createdAt', 'DESC']
+            ],
+            include: [
+                {
+                    model: User,
+                    as: 'user',
+                    attributes: ['name'],
+                }
             ]
-        })
-        return posts;
+        });
+
+        const response = posts.map(post => ({
+           ...post.toJSON(),
+           displayName: post.isAnonymous ? 'Anonymous' : post.user?.name,
+           user: post.isAnonymous ? null : post.user,
+           userId: post.isAnonymous ? null : post.userId,
+        }));
+
+        return response;
     }
     async findPostsByUserId(userId) {
         const posts = await Post.findAll({
             where: {userId: userId},
             order: [
                 ['createdAt', 'DESC']
+            ],
+            include: [
+                {
+                    model: User,
+                    as: 'user',
+                    attributes: ['name'],
+                }
             ]
         });
         return posts;
