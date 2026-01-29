578f78f8854f4b50cb9329b23b114489bae10c0b
MANISH6148 <manishbobburi6148@gmail.com>
2026-01-30 00:03:55 +0530

    feat(models): define Post-User belongsTo  association

--- src/models/post.js
+++ src/models/post.js
@@ -11,8 +11,13 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.belongsTo(models.User, {
+        foreignKey: 'userId',
+        as: 'user'
+      });
     }
   }
+
   Post.init({
     userId: DataTypes.INTEGER,
     company: DataTypes.STRING,
