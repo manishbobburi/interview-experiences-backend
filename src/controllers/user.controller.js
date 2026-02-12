1a1af037f220123f594e2420b5bfa71d88ca3692
MANISH6148 <manishbobburi6148@gmail.com>
2026-02-12 23:21:44 +0530

    feat(controllers): ad updateUserDetails and changePassword functions in user controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -30,8 +30,48 @@ async function signIn(req, res, next) {
 
 async function getMe(req, res, next) {
     try {
-        const user = await userService.getMe(req.user);
+        const user = await userService.getMe(req.userId);
         return sendSuccess(res, user, "Fetched user successfully", StatusCodes.OK);
+    } catch (err) {
+        next(err);
+    }
+}
+
+async function updateUserDetails(req, res, next) {
+    try {
+        const updateFields = {};
+
+        if (req.body.name !== undefined) {
+            updateFields.name = req.body.name.trim();
+        }
+
+        if (req.body.email !== undefined) {
+            updateFields.email = req.body.email.trim();
+        }
+
+        const response = await userService.updateUserDetails(req.userId, updateFields);
+        return sendSuccess(res, response, "User details updated successfully", StatusCodes.OK);
+    } catch (err) {
+        next(err);
+    }
+}
+
+async function changePassword(req, res, next) {
+    try {
+        const { oldPassword, newPassword } = req.body;
+
+        if (!oldPassword || !newPassword) {
+            return res.status(400).json({ message: "Passwords required" });
+        }
+
+        const passwords = {
+            oldPassword: oldPassword.trim(),
+            newPassword: newPassword.trim(),
+        };
+
+        const response = await userService.changePassword(req.userId, passwords);
+        return sendSuccess(res, response, "Password changed successfully", StatusCodes.OK);
+
     } catch (err) {
         next(err);
     }
@@ -41,4 +81,6 @@ module.exports = {
     signUp,
     signIn,
     getMe,
+    updateUserDetails,
+    changePassword,
 }
