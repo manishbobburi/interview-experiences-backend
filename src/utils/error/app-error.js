41acb9bd6be9f0c42f24408697259938da6eed99
MANISH6148 <manishbobburi6148@gmail.com>
2026-02-12 23:28:19 +0530

    add error code and error captureStrackTrace

--- src/utils/error/app-error.js
+++ src/utils/error/app-error.js
@@ -1,8 +1,13 @@
 class AppError extends Error {
-    constructor(message, statusCode) {
+    constructor(message, statusCode, errorCode = "INTERNAL_ERROR", details = null) {
         super(message);
-        this.statusCode = statusCode;
+
+        this.statusCode = statusCode || 500;
         this.explanation = message;
+        this.errorCode = errorCode;
+        this.details = details
+
+        Error.captureStackTrace(this, this.constructor);
     }
 }
 
