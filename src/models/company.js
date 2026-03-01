c2ee76618015a47e0fe9cb18885b697f6e97c7c8
MANISH6148 <manishbobburi6148@gmail.com>
2026-03-01 17:25:57 +0530

    feat(company): add model Comapny

---
+++ src/models/company.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Company extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.hasMany(models.Post, {
+        foreignKey: "companyId",
+        as: "posts",
+      });
+    }
+  }
+  Company.init({
+    name: DataTypes.STRING,
+    logoUrl: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Company',
+  });
+  return Company;
+};
