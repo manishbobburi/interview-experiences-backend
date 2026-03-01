44e5e28ceaefbcbad53bcd0eaf4e78f07b4b4b5a
MANISH6148 <manishbobburi6148@gmail.com>
2026-03-02 00:10:55 +0530

    feat(repositories): add company repository

---
+++ src/repositories/company.repository.js
@@ -0,0 +1,10 @@
+const CrudRepository = require("./crud.repository");
+const { Company } = require("../models");
+
+class CompanyRepository extends CrudRepository {
+    constructor() {
+        super(Company);
+    }
+}
+
+module.exports = CompanyRepository;
--- src/repositories/index.js
+++ src/repositories/index.js
@@ -3,4 +3,5 @@ module.exports = {
     PostRepository: require("./post.repository"),
     CommentRepository: require("./comment.repository"),
     VoteRepository: require("./vote.repository"),
+    CompanyRepository: require("./company.repository"),
 }
