9394fd4ff5939980e168ee78298f45c54fcf1308
MANISH6148 <manishbobburi6148@gmail.com>
2026-03-11 23:08:08 +0530

    feat(routes): protect add-company route with auth & authorization checks

--- src/routes/v1/companies.routes.js
+++ src/routes/v1/companies.routes.js
@@ -2,9 +2,21 @@ const express = require("express");
 const router = express.Router();
 
 const { companyController } = require("../../controllers");
+const { companyMiddleware } = require("../../middleware");
+const { authMiddleware, authorizeMiddleware } = require("../../middleware");
+const ROLES = require("../../utils/constants/roles");
 
-router.get("/", 
+router.get("/",
+    authMiddleware.checkAuth,
+    authorizeMiddleware.authorizeRoles(ROLES.USER, ROLES.ADMIN),
     companyController.getCompanies
+)
+
+router.post("/",
+    authMiddleware.checkAuth,
+    authorizeMiddleware.authorizeRoles(ROLES.ADMIN),
+    companyMiddleware.validateAddCompanies,
+    companyController.addCompanies
 )
 
 module.exports = router;
