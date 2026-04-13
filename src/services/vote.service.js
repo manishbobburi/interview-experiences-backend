3b5d980d32213278bf64ad5bae8ca1daea5ce4a4
MANISH6148 <manishbobburi6148@gmail.com>
2026-04-13 22:53:56 +0530

    feat(votes): add like and un-like posts

--- src/services/vote.service.js
+++ src/services/vote.service.js
@@ -1,24 +1,24 @@
-const { VoteRepository } = require("../repositories");
+const { VoteRepository, PostRepository } = require("../repositories");
 
 const voteRepository = new VoteRepository();
+const postRepository = new PostRepository();
 
-async function upVote(data) {
-    const response = await voteRepository.create(data);
-    return response;
-}
+async function toggleLike(data) {
+    const existingVote = await voteRepository.model.findOne({
+        where: { userId: data.userId, postId: data.postId }
+    });
 
-async function downVote(data) {
-    const response = await voteRepository.create(data);
-    return response;
-}
-
-async function removeVote(data) {
-    const response = await voteRepository.destroy(data);
-    return response;
+    if (existingVote) {
+        await voteRepository.model.destroy({ where: { id: existingVote.id } });
+        await postRepository.model.decrement("upVotes", { by: 1, where: { id: data.postId } });
+        return { liked: false };
+    } else {
+        await voteRepository.model.create({ userId: data.userId, postId: data.postId, vote: 1 });
+        await postRepository.model.increment("upVotes", { by: 1, where: { id: data.postId } });
+        return { liked: true };
+    }
 }
 
 module.exports = {
-    upVote,
-    downVote,
-    removeVote,
+    toggleLike,
 }
